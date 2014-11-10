a257b8332b197dc856ac6c94c791fbeba268e4d5
Ray Chun <m5chun@uwaterloo.ca>
2014-11-10 18:47:32 -0500

    our part of the project

---
+++ GUI.py
@@ -0,0 +1,15 @@
+#Scribbler GUI
+
+from Tkinter import *
+
+#create the window
+root = Tk()
+
+#modify root window
+root.title("Simple GUI")
+root.geometry("200x100")
+
+#kick off the event loop
+root.mainloop
+
+
